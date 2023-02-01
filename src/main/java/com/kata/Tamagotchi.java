f0377a2f6c2003ee08a680bb57579463a046f600
Enol-Igareta <enol1code@gmail.com>
2023-02-01 09:20:54 +0100

    primera iteraccion acabada

--- src/main/java/com/kata/Tamagotchi.java
+++ src/main/java/com/kata/Tamagotchi.java
@@ -1,9 +1,52 @@
 package com.kata;
 
 public class Tamagotchi {
-    
+
     int mood = 4;
-    int hunger = 4;
+    int hunger = 8;
     int energy = 4;
+    
+
+    public Tamagotchi() {
+        this.mood = mood;
+        this.hunger = hunger;
+        this.energy = energy;
+    }
+    public int getMood() {
+        return mood;
+    }
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+    public int getHunger() {
+        return hunger;
+    }
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+    public int getEnergy() {
+        return energy;
+    }
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+    
+    public void eat(){
+        if(this.getHunger()>1)this.setHunger(this.getHunger()-2);
+        
+        if(this.getEnergy()<10) this.setEnergy(this.getEnergy()+1);
+    } 
+
+    public void sleep(){
+        if(this.getEnergy()<10) this.setEnergy(this.getEnergy()+2);
+    } 
+
+    public void play(){
+        if(this.getHunger()<10 && this.getEnergy()<10 && this.getMood()<10){
+            this.setHunger(this.getHunger()+1);
+            this.setEnergy(this.getEnergy()-1);
+            this.setMood(this.getMood()+1);
+        };
+    }
 
 }
--- src/test/java/com/kata/TamagotchiTest.java
+++ src/test/java/com/kata/TamagotchiTest.java
@@ -1,15 +1,63 @@
 package com.kata;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+// import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class TamagotchiTest {
     
+    Tamagotchi gochi = new Tamagotchi();
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void eat_low_hunger()
     {
-        assertTrue( true );
+        gochi.eat();
+        gochi.eat();
+        gochi.eat();
+        assertEquals(2, gochi.getHunger());
+    }
+
+    @Test
+    public void eat_raises_energy()
+    {
+        gochi.eat();
+        gochi.eat();
+        gochi.eat();
+        gochi.eat();
+        assertEquals(8, gochi.getEnergy());
+    }
+
+    @Test
+    public void sleep_raises_energy()
+    {
+        gochi.sleep();
+        gochi.sleep();
+        assertEquals(8, gochi.getEnergy());
+    }
+
+    @Test
+    public void play_raises_hunger()
+    {
+        gochi.play();
+        gochi.play();
+        assertEquals(10, gochi.getHunger());
+    }
+
+    @Test
+    public void play_raises_mood()
+    {
+        gochi.play();
+        gochi.play();
+        assertEquals(6, gochi.getMood());
+    }
+
+    @Test
+    public void play_low_energy()
+    {
+        gochi.play();
+        gochi.play();
+        assertEquals(2, gochi.getEnergy());
     }
 
 }
